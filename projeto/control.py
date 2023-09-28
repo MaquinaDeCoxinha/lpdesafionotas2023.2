12ddc050b4915c0c303193128cda898b2ffd160c
MaquinaDeCoxinha <jvpvizeu@gmail.com>
2023-09-28 14:55:35 -0300

    added basic route control

--- projeto/control.py
+++ projeto/control.py
@@ -1,1 +1,22 @@
 # requests e rotas
+from flask import Flask, render_template, request, redirect, url_for, session
+from model import *
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return redirect(url_for('login'))
+'''
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if username in users and users[username] == password:
+            session['username'] = username
+            return redirect(url_for('view_notes'))
+    return render_template('login.html')
+'''
+if __name__ == '__main__':
+    app.run(debug=True)
